d0fa29cc1bd3688b59ee8a6a682062f6cdb464ef
gripsmoke <gripsmoke@gmail.com>
2018-04-18 12:15:12 -0700

    First commit. Already have some stuff set up. Interactive keyboard toggles a display of the major and minor chord progressions for the note chosen.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,26 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { NotesComponent } from './notes/notes.component';
+import { ChordsComponent } from './chords/chords.component';
+import { PianoComponent } from './piano/piano.component';
 
+import { RootnoteService } from './rootnote.service';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NotesComponent,
+    ChordsComponent,
+    PianoComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [
+    RootnoteService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
